69e0ee863c9e07dccf22f7e2cb67c09bb7f728d4
Joe Gelay <joegelay@gmail.com>
2020-01-12 21:11:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].Join()
+     programmer_hash[:dennis_ritchie][:languages].Join("")
 end
 
 def adding_matz
